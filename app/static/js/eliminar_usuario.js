b0822a8ec1de508104f34da970beac10b443a750
xam1002 <74177193+xam1002@users.noreply.github.com>
2022-06-10 18:32:26 +0200

    Adición de una cabecera

--- app/static/js/eliminar_usuario.js
+++ app/static/js/eliminar_usuario.js
@@ -1,3 +1,7 @@
+/**
+ * Implementa la ventana de confirmación para eliminar un usuario.
+ * @param e Evento.
+ */
 function confirmacion(e) {
     if (!confirm("¿Está seguro que desea eliminar el usuario?"))
         e.preventDefault();
