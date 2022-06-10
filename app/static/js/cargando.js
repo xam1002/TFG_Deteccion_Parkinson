c1808a301e4965a5ee9e52837ff1cc3ff0f04d04
xam1002 <74177193+xam1002@users.noreply.github.com>
2022-06-10 18:30:55 +0200

    Corrección de errores y adición de cabecera

--- app/static/js/cargando.js
+++ app/static/js/cargando.js
@@ -1,9 +1,22 @@
+/**
+ * Implementa la función para que aparezca el símbolo de cargando al realizar una predicción.
+ */
 function cargando() {
-    var contenedor = document.getElementById('contenedor_carga');
-    var bienvenida = document.getElementById('bienvenida');
-    var subida = document.getElementById('subida');
-    contenedor.style.display = 'block';
-    bienvenida.style.visibility = 'hidden';
-    bienvenida.style.opacity = '0';
-    subida.style.visibility = 'hidden';
+    const area = document.querySelector(".arrastrar");
+    const input = area.querySelector("#archivo");
+    var mano = document.predecir.mano;
+    var sexo = document.predecir.sexo;
+    var archivo = document.predecir.archivo;
+    console.log(input.files);
+    extensiones = [".ogm", ".wmv", ".mpg", ".webm", ".ogv", ".mov", ".asx", ".mpeg", ".mp4", ".m4v", ".avi", ".mkv"]
+    if (mano.value != '' && sexo.value != '' && (archivo.value != '' && !extensiones.includes(archivo.value) 
+    || input.files != '' && !extensiones.includes(input.files))) {
+        var contenedor = document.getElementById('contenedor_carga');
+        var bienvenida = document.getElementById('bienvenida');
+        var subida = document.getElementById('subida');
+        contenedor.style.display = 'block';
+        bienvenida.style.visibility = 'hidden';
+        bienvenida.style.opacity = '0';
+        subida.style.visibility = 'hidden';
+    }
 }
